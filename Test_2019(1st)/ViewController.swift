0ca45e03ed018ba1c8c6b3f18ab04cc4ac443ad9
BraverChiang <huanfa2020@gmail.com>
2018-12-02 16:25:59 +0800

    Initial Commit

---
+++ Test_2019(1st)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_2019(1st)
+//
+//  Created by Braver Chiang on 12/2/18.
+//  Copyright © 2018 Braver Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
