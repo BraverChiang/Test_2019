6dce632cd6533321215a97b2075161f51665ecc5
BraverChiang <huanfa2020@gmail.com>
2018-12-02 16:36:33 +0800

    0.2 A 豆子你好

--- Test_2019(1st)/ViewController.swift
+++ Test_2019(1st)/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("hello, yongge")
+        print("豆子, 你好啊")
     }
 
 
